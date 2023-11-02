039cd65049316be96cf88c771501efcb1e74d9d1
Nikolay Khechumov <ngkhechumov@avito.ru>
2023-11-02 15:16:48 +0300

    Notice

--- deepsecrets/__main__.py
+++ deepsecrets/__main__.py
@@ -1,5 +1,15 @@
 import sys
+from deepsecrets import logger
 
-from deepsecrets.cli import DeepSecretsCliTool
+message = \
+'\n'\
+'================== DEPRECATION NOTICE ==================\n' \
+'Active developent was moved to the creator\'s fork at\n' \
+'      https://github.com/ntoskernel/deepsecrets\n\n' \
+'     This repository will no longer receive updates \n'\
+'========================================================\n'
 
-sys.exit(DeepSecretsCliTool(sys.argv).start())
+
+logger.error(message)
+
+sys.exit()
